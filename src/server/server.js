b11e4554dd8f768fc49227f8972732097a8ac4f5
Kyle Cheung <cheung.kyle1@gmail.com>
2022-01-17 22:32:02 -0500

    added node server

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,22 @@
 // environment variables create .env file to use.
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const PORT = process.env.SERVER_PORT || 8888;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => console.log("db connected successfully"))
+  .catch((err) => console.log(err));
 
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../dist")));
 
-require('./routes/exampleApi')(app);
+require("./routes/exampleApi")(app);
 
- app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
